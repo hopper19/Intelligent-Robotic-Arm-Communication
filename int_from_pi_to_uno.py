0d8d68ccb54b05f1e8abf3df7de57a4aa9710794
Cuong Nguyen <cuongvnus2001@gmail.com>
2023-04-11 16:39:24 -0400

    time sleep now sleeps for precisely 1ms

--- int_from_pi_to_uno.py
+++ int_from_pi_to_uno.py
@@ -13,11 +13,12 @@ import serial # Module needed for serial communication
 import time # Module needed to add delays in the code
  
 # Set the port name and the baud rate. This baud rate should match the baud rate set on the Arduino.
-# ser = serial.Serial('COM9', 115200, timeout=1)
+ser = serial.Serial('COM9', 115200, timeout=1)
 file = open("Cuong-Frontal-Blink-2.txt", "r")
+send_string = '1'
   
 # Get rid of garbage/incomplete data
-# ser.flush()
+ser.flush()
 
 line = file.readline()
 while line:
@@ -30,17 +31,17 @@ print("Reading data file...")
 while line:
   if "#" in line:
     idx = line.find('#')
-    send_string = line[idx+2:].strip()
+    time_stamp = line[idx+2:].strip()
     # Send the string. Make sure you encode it before you send it to the Arduino
-    # ser.write(send_string.encode('utf-8'))
-    print("Blinked at", send_string,  "and notified Arduino")
+    ser.write(send_string.encode('utf-8'))
+    print("Blinked at", time_stamp,  "and notified Arduino")
 
   # Wait for a set amount of time before sending the next integer
   time.sleep(0.001)
   line = file.readline()
 
 # Close the serial port
-# ser.close()
+ser.close()
 
 
 

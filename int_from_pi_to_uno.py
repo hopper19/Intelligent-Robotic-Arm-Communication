eeeec8fc077639d05b50aca0da440a8ad18e554a
Cuong Nguyen <cuongvnus2001@gmail.com>
2023-04-11 14:50:08 -0400

    added reading file functionality

--- int_from_pi_to_uno.py
+++ int_from_pi_to_uno.py
@@ -12,39 +12,35 @@
 import serial # Module needed for serial communication
 import time # Module needed to add delays in the code
  
-# Set the port name and the baud rate. This baud rate should match the
-# baud rate set on the Arduino.
-# Timeout parameter makes sure that program doesn't get stuck if data isn't
-# being received. After 1 second, the function will return with whatever data
-# it has. The readline() function will only wait 1 second for a complete line 
-# of input.
-ser = serial.Serial('COM9', 115200, timeout=1)
+# Set the port name and the baud rate. This baud rate should match the baud rate set on the Arduino.
+# ser = serial.Serial('COM9', 115200, timeout=1)
+file = open("Cuong-Frontal-Blink-2.txt", "r")
   
 # Get rid of garbage/incomplete data
 # ser.flush()
 
-# Wait for the Arduino to initialize its serial communication
-time.sleep(5)
- 
-# Infinite loop
-while (1):
+line = file.readline()
+while line:
+  if line[0].isdigit():
+    break
+  line = file.readline()
+print("Reading data file...")
 
-  send_string = '1'
-  # send_string += "\n"
-
-  # Send the string. Make sure you encode it before you send it to the Arduino
-  ser.write(send_string.encode('utf-8'))
-  print("Just blinked and notified Arduino")
-
-  # Receive data from the Arduino
-  # if ser.in_waiting > 0:
-  # receive_string = ser.readline().decode('utf-8', 'replace').rstrip()
-
-  # Print the data received from Arduino to the terminal
-  # print("Regurgitated by Arduino:")
-  # print(receive_string + "\n")
+# Read until there is data left in the file
+while line:
+  if "#" in line:
+    idx = line.find('#')
+    send_string = line[idx+2:].strip()
+    # Send the string. Make sure you encode it before you send it to the Arduino
+    # ser.write(send_string.encode('utf-8'))
+    print("Blinked at", send_string,  "and notified Arduino")
 
   # Wait for a set amount of time before sending the next integer
-  time.sleep(5)
+  time.sleep(0.001)
+  line = file.readline()
+
+# Close the serial port
+# ser.close()
+
 
 
